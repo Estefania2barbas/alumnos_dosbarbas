4146fb81e4378899eccf31a12c38fdd323b51247
aaron cruces <aaroncruces@gmail.com>
2023-07-24 15:10:08 -0400

    prisma

--- src/app/components/loadScripts.tsx
+++ src/app/components/loadScripts.tsx
@@ -8,7 +8,6 @@ export default function LoadScripts() {
     <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js" > </script> 
     <script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js"  > </script> */}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossOrigin="anonymous"></script>
-    
    </>
   )
 }
--- src/lib/proyectoprograma/getAllProyectoPrograma.ts
+++ src/lib/proyectoprograma/getAllProyectoPrograma.ts
@@ -1,5 +1,12 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+
 export default async function getAllProyectoPrograma(): Promise<ProyectoPrograma[]> {
     const res = await fetch("http://localhost:7081/mocks/proyectos.json")
+    const prismaProyectos=await prisma.proyecto.findMany()
+    console.log(prismaProyectos);
+    
     if(!res.ok) throw new Error("error obteniendo proyecto_programas desde backend")
     return res.json()
 }
